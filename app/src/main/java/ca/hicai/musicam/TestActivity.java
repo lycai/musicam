de82a6f535de9b6db773afe173f310bdfa329ed8
Yves Li <y@ves.li>
2015-01-31 01:30:21 -0500

    Fix some package silliness

--- app/src/main/java/ca/hicai/musicam/TestActivity.java
+++ app/src/main/java/ca/hicai/musicam/TestActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import me.yvesli.testbed.R;
+import ca.hicai.musicam.R;
 
 
 public class TestActivity extends ActionBarActivity {
