844c78ef8d651758566254d42d7abe1145f669c6
Yves Li <y@ves.li>
2015-01-31 18:23:08 -0500

    second commit

--- app/src/main/java/ca/hicai/musicam/SoundGenerator.java
+++ app/src/main/java/ca/hicai/musicam/SoundGenerator.java
@@ -5,57 +5,337 @@ import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.util.Log;
 
-/**
- * Created by yves on 30/01/15.
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class SoundGenerator {
+    // Number of samples used to fade between tones.
+    private static final int FADE_LEN = 10;
+
+    private static interface Note {
+        public int getDuration();
+        public double getAmplitude();
+        public double getFrequency();
+        public int getProgress();
+        public int progress();
+        public Note reset();
+        public Note fade(Note n);
+        public double getFadeFreq();
+        public double getFadeAmp();
+    }
+
+    public static abstract class AbstractNote implements Note {
+        protected int duration; // duration in hundredths of a second
+        protected int progress; // duration played
+        protected double ffreq;
+        protected double famp;
+
+        public AbstractNote(int duration) {
+            this.duration = duration;
+        }
+
+        @Override
+        public int getDuration() {
+            return duration;
+        }
+
+        @Override
+        public int progress() {
+            return progress++;
+        }
+
+        @Override
+        public int getProgress() {
+            return progress;
+        }
+
+        @Override
+        public Note reset() {
+            progress = 0;
+            return this;
+        }
+
+        @Override
+        public double getFadeFreq() {
+            return ffreq;
+        }
+
+        @Override
+        public double getFadeAmp() {
+            return famp;
+        }
+
+        @Override
+        public Note fade(Note n) {
+            ffreq = n.getFrequency();
+            famp = n.getAmplitude();
+            return reset();
+        }
+    }
+    private static class ConcreteNote extends AbstractNote {
+        private int pitch;        // distance from A4 in semitones
+        private double amplitude; // amplitude from 0 to 1
+
+        public ConcreteNote(int pitch, int duration, double amplitude) {
+            super(duration);
+            this.pitch = pitch;
+            this.amplitude = amplitude;
+        }
+
+        // No amplitude = max volume
+        public ConcreteNote(int pitch, int duration) {
+            this(pitch, duration, 1.0);
+        }
+
+        // No pitch / amplitude = silence
+        public ConcreteNote(int duration) {
+            this(0, duration, 0);
+        }
+
+        public int getPitch() {
+            return pitch;
+        }
+
+        @Override
+        public double getAmplitude() {
+            return amplitude;
+        }
+
+        @Override
+        public double getFrequency() {
+            return Math.pow(2, pitch / 12.0) * 440;
+        }
+
+        public void setProgress(int progress) {
+            this.progress = progress;
+        }
+
+        public void setDuration(int duration) {
+            this.duration = duration;
+        }
+    }
+
+    private static class Track implements Iterable<Note> {
+        private List<Note> data = new LinkedList<>();
+        private int totalDuration = 0;
+
+        private static final Note SILENCE = new ConcreteNote(0, 1, 0);
+
+        @Override
+        public Iterator<Note> iterator() {
+            return new Iterator<Note>() {
+                // The progress of the current Note.
+                private Iterator<Note> delegate = data.iterator();
+                private Note current = delegate.hasNext() ? delegate.next() : SILENCE;
+
+                @Override
+                public boolean hasNext() {
+                    return true;
+                }
+
+                @Override
+                public Note next() {
+                    if (current == SILENCE) {
+                        return SILENCE;
+                    }
+                    if (current.progress() > current.getDuration() - FADE_LEN) {
+                        current = delegate.hasNext() ? delegate.next().fade(current) : SILENCE;
+                    }
+                    return current;
+                }
+
+                @Override
+                public void remove() {
+                    throw new IllegalStateException("can't remove notes from Track iterator");
+                }
+            };
+        }
+
+        public int getTotalDuration() {
+            return totalDuration;
+        }
+
+        public void addNote(int pitch, int duration, double amplitude) {
+            totalDuration += duration;
+            data.add(new ConcreteNote(pitch, duration, amplitude));
+        }
+
+        public void addNote(Note note) {
+            totalDuration += note.getDuration();
+            data.add(note);
+        }
+
+        public void clear() {
+            totalDuration = 0;
+            data.clear();
+        }
+
+        public void reset() {
+            if (data.size() > 0) {
+                data.get(0).reset();
+            }
+        }
+    }
 
     private static final String TAG = "SoundGenerator";
     private static final int SAMPLE_RATE = 44100;
-    private AudioTrack track;
 
-    public SoundGenerator() {
-        int minBuf = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT);
-        track = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, minBuf, AudioTrack.MODE_STREAM);
-        Log.d(TAG, "Creating AudioTrack with minimum buffer " + minBuf);
+    private Track[] tracks;
+
+    public SoundGenerator(int numTracks) throws NullPointerException, IllegalArgumentException {
+        if (numTracks < 1) {
+            throw new IllegalArgumentException("numTracks must be positive");
+        }
+
+        tracks = new Track[numTracks];
+        for (int i = 0; i < numTracks; i++) {
+            tracks[i] = new Track();
+        }
+
+        Log.d(TAG, "New SoundGenerator created with " + numTracks + " track(s).");
     }
 
-    public void dispose() {
-        track.release();
+    public void addNote(int track, int pitch, int duration, double amplitude) throws IllegalArgumentException {
+        if (track < 0 || track >= tracks.length) {
+            throw new IllegalArgumentException("track out of bounds: got " + track + ", expected [0," + tracks.length + "]");
+        }
+        // We permit all notes on the piano. There are 48 semitones below A4, and 39 above.
+        if (pitch < -48 || pitch > 39) {
+            throw new IllegalArgumentException("pitch out of bounds: got " + pitch + ", expected [-48,39]");
+        }
+        if (duration <= 0) {
+            throw new IllegalArgumentException("duration out of bounds: got " + duration + ", expected (0, +inf)");
+        }
+        if (amplitude < 0 || amplitude > 1) {
+            throw new IllegalArgumentException("amplitude out of bounds: got " + amplitude + ", expected [0,1]");
+        }
+        Log.d(TAG, "add note to track " + track + "; pitch: " + pitch + "; duration: " + duration + "; amplitude: " + amplitude);
+        tracks[track].addNote(pitch, duration, amplitude);
     }
 
-    public void play(final double freq, final int length) {
+    public void addSilence(int track, int duration) {
+        addNote(track, 0, duration, 0);
+    }
+
+    public void play(/*final double freq, final int length*/) {
+        //Log.d(TAG, "Playing wave with frequency " + freq + " for " + length + " second(s)");
+
         new Thread(new Runnable() {
+            final Queue<short[]> buffer = new LinkedList<>();
+            AtomicBoolean doneBuffering = new AtomicBoolean(false);
+
             @Override
             public void run() {
-                Log.d(TAG, "Playing wave with frequency " + freq + " for " + length + " second(s)");
+                int bufSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT);
+                AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT, bufSize, AudioTrack.MODE_STREAM);
+//                if (audioTrack == null) {
+//                    throw new NullPointerException("failed to create AudioTrack");
+//                }
 
-                // generate five seconds worth of sound, in one-second increments
-                short[] buf = new short[SAMPLE_RATE * length];
+                final Thread bufferWriter = new Thread() {
+                    @Override
+                    public synchronized void run() {
+                        // First, find the total duration and set up the iterators. The total
+                        // duration is equal to the maximum of the total durations of all tracks.
+                        int totalDuration = 0, numTracks = tracks.length;
+                        List<Iterator<Note>> iters = new ArrayList<>(numTracks);
+                        for (Track t : tracks) {
+                            iters.add(t.iterator());
+                            totalDuration = Math.max(totalDuration, t.getTotalDuration());
+                            t.reset();
+                        }
 
-                for (int i = 0; i < SAMPLE_RATE * length; i++) {
-                    double sample = Math.sin(2 * Math.PI * freq * i / SAMPLE_RATE);
-                    // clamp sample
-                    if (sample > 1) {
-                        sample = 1;
-                    } else if (sample < -1) {
-                        sample = -1;
+                        final int FADE_SAMPLES = FADE_LEN * SAMPLE_RATE / 100;
+
+                        for (int time = 0; time < totalDuration; time++) {
+                            // Mix the samples. We just add together all the samples across all
+                            // tracks, then divide by the number of tracks.
+                            double[] samples = new double[SAMPLE_RATE / 100];
+                            for (Iterator<Note> it : iters) {
+                                Note note = it.next();
+                                double amp = note.getAmplitude(), freq = note.getFrequency(),
+                                    famp = note.getFadeAmp(), ffreq = note.getFadeFreq();
+                                int prog = note.getProgress();
+                                for (int i = 0; i < SAMPLE_RATE / 100; i++) {
+                                    int sampleNum = time * (SAMPLE_RATE / 100) + i;
+                                    double sample = amp * Math.sin(2 * Math.PI * freq * sampleNum / SAMPLE_RATE);
+                                    if (prog < FADE_LEN && famp > 0.01) {
+                                        sample *= (double)(FADE_SAMPLES - prog * SAMPLE_RATE / 100 - i) / FADE_SAMPLES;
+                                        sample += famp * Math.sin(2 * Math.PI * ffreq * sampleNum / SAMPLE_RATE)
+                                            * (prog * SAMPLE_RATE / 100 + i) / FADE_SAMPLES;
+                                        sample /= 2;
+                                     }
+                                    samples[i] += sample;
+                                }
+                            }
+
+                            // Convert the samples into shorts and queue them for writing.
+                            short[] buf = new short[SAMPLE_RATE / 100];
+                            for (int i = 0; i < buf.length; i++) {
+                                buf[i] = (short) (samples[i] / numTracks * 32767);
+                            }
+                            synchronized (buffer) {
+                                buffer.add(buf);
+                            }
+//                            Log.d(TAG, "wrote sample + notified -- " + time);
+                            notify();
+                        }
+                        Log.d(TAG, "done buffering samples; " + totalDuration + " written");
+                        doneBuffering.set(true);
                     }
-                    buf[i] = (short)(sample * 32767);
+                };
+                bufferWriter.start();
+
+                synchronized (bufferWriter) {
+                    audioTrack.play();
+
+                    int written = 0;
+
+                    while (true) {
+                        // If our buffer is empty, wait for it to be populated before writing.
+                        if (buffer.isEmpty()) {
+                            if (doneBuffering.get()) {
+                                break;
+                            }
+                            try {
+                                Log.d(TAG, "player waiting for bufferWriter");
+                                bufferWriter.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        short[] buf;
+                        synchronized (buffer) {
+                            buf = buffer.remove();
+                        }
+                        written += audioTrack.write(buf, 0, buf.length);
+                    }
+                    Log.d(TAG, written + " total bytes written to audio buffer");
+                    audioTrack.stop();
                 }
-                track.play();
-                Log.d(TAG, "Done writing buffer");
-                int status = track.write(buf, 0, buf.length);
-                Log.d(TAG, "Write returned with status " + status);
+                audioTrack.release();
             }
         }).start();
+
+        /*
+        short[] buf = new short[SAMPLE_RATE];
+        for (int i = 0; i < SAMPLE_RATE; i++) {
+            double sample = Math.sin(2 * Math.PI * 440 * i / SAMPLE_RATE);
+            buf[i] = (short) (sample * 32767);
+        }
+        */
     }
 
-    public void playRandom(int length) {
-        int note = (int)(Math.random() * 21) - 10;
-        Log.d(TAG, "Playing random tone -- " + note);
-        play(Math.pow(2, note / 12.0) * 440, length);
+    public void clear() {
+        for (Track t : tracks) {
+            t.clear();
+        }
     }
 }
---
+++ app/src/main/java/ca/hicai/musicam/SoundPlayerActivity.java
@@ -0,0 +1,132 @@
+package ca.hicai.musicam;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class SoundPlayerActivity extends ActionBarActivity {
+
+    private static final String TAG = "SoundPlayerActivity";
+    private static final int NUM_TRACKS = 2;
+
+    private SoundGenerator soundGenerator;
+    private boolean wasSong = false;
+    private Button addButton;
+    private Button playButton;
+    private EditText track;
+    private EditText pitch;
+    private EditText duration;
+    private EditText amplitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundplayer);
+
+        soundGenerator = new SoundGenerator(NUM_TRACKS);
+
+        addButton = (Button) findViewById(R.id.addButton);
+        playButton = (Button) findViewById(R.id.playbutton);
+        track = (EditText) findViewById(R.id.track);
+        pitch = (EditText) findViewById(R.id.pitch);
+        duration = (EditText) findViewById(R.id.duration);
+        amplitude = (EditText) findViewById(R.id.amplitude);
+        /*
+        button.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("TestActivity", "button clicked. freq: " + freq.getText().toString() + "; len: " + len.getText().toString());
+                    int n = 0, dur = 1;
+                    try {
+                        n = Integer.parseInt(freq.getText().toString());
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                    try {
+                        dur = Integer.parseInt(len.getText().toString());
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                    if (n > 26 && n < 4000) {
+                        soundGenerator.play(n, dur);
+                    } else {
+                        soundGenerator.playRandom(dur);
+                    }
+                }
+            }
+        );
+        */
+    }
+
+    public void addNote(View v) {
+        Log.d(TAG, "AddNote clicked");
+        int t, p, d;
+        double a;
+        try {
+            t = Integer.parseInt(track.getText().toString());
+            p = Integer.parseInt(pitch.getText().toString());
+            d = Integer.parseInt(duration.getText().toString());
+            a = Double.parseDouble(amplitude.getText().toString());
+
+            Log.d(TAG, "Parsed values: " + p + ", " + d + ", " + a);
+
+            if (wasSong) {
+                wasSong = false;
+                soundGenerator.clear();
+            }
+            soundGenerator.addNote(t, p, d, a);
+        } catch (IllegalArgumentException ex) {
+            Toast.makeText(getApplicationContext(), "Invalid input!", Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+    public void playSong(View v) {
+        soundGenerator.play();
+    }
+
+    public void makeSong(View v) {
+        soundGenerator.clear();
+        int[] notes = { 3, 5, 7, 8, 10, 12, 14, 15 };
+        for (int i = 0; i < notes.length; i++) {
+            soundGenerator.addNote(0, notes[i], 50, 0.6 + 0.4 * i / 8);
+            soundGenerator.addNote(1, notes[notes.length - i - 1], 50, 0.6 + 0.4 * i / 8);
+        }
+        // soundGenerator.addSilence(0, 100);
+        for (int i = notes.length - 1; i >= 0; i--) {
+            soundGenerator.addNote(0, notes[i], 50, 0.6 + 0.4 * i / 8);
+            soundGenerator.addNote(1, notes[notes.length - i - 1], 50, 0.6 + 0.4 * i / 8);
+        }
+        wasSong = true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_soundplayer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
